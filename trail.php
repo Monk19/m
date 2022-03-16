15b2d7fb3420c50bda1f735c8407cf229cf7ad4f
Monk19 <ravigadu.19@gmail.com>
2022-03-16 09:06:59 +0530

    first commit

---
+++ trail.php
@@ -0,0 +1,17 @@
+<?php
+/*
+echo "hello world \n";
+// A PHP script is executed on the server, and the plain HTML result is sent back to the browser.
+
+$aVariable = "Namik";
+$AVariable = 1;
+
+#echo $AVariable." ".$aVariable;
+*/
+$animes = array("dbz", "dbz super", "Death note");
+// echo var_dump($animes); var_dump($variable Name) can be used to check Name of the variable
+// echo $animes[0];
+// echo PHP_INT_MAX;
+// echo PHP_INT_MIN;
+echo PHP_INT_SIZE + abs(PHP_INT_MIN)."\n";
+echo pi();
